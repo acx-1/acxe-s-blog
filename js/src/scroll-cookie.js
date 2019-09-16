094f6d73b47f4fba119a1b7af504539c5ffd4249
acxer <qq772396251@163.com>
2019-09-16 16:02:33 +0800

    Site updated: 2019-09-16 16:02:12

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 15 2019 20:02:46 GMT+0800 (GMT+08:00)
+// build time:Mon Sep 16 2019 16:01:15 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]==i&&$(window).scrollTop(e[0])}});
 //rebuild by neat 
